fc3046adf0a75ed982aa14cf0b0e0c14a9b8386c
Denis Ansah <denis.ansah@yahoo.com>
2024-04-02 01:43:58 -0400

    Initial Commit

---
+++ AdwumaX/AdwumaXApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdwumaXApp.swift
+//  AdwumaX
+//
+//  Created by Denis on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct AdwumaXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
